7ad0d38022fdc485dfe8e1ba354d8154f15a596b
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-06-15 12:38:55 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the required line
+ * Return: 0 (at the end)
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
