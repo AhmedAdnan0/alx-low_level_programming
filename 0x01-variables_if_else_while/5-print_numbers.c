9407732afb6de4bcabf813f67f63944909364c07
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-06-16 14:41:31 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This function prints all digits
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
