f7663daa84a64a5531fc284644e6a492c0695199
AhmedAdnan0 <ahmed.adnan.m93@gmail.com>
2023-06-17 20:17:09 +0000

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- * Return: 1 (Success)
+ * main - prints: "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *Return: 1 (Success)
  */
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	int I = sizeof(quote);
-	int i;
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	for (i = 0; i < I; ++i)
-	{
-		putchar(quote[i]);
-	}
-	putchar('\n');
+	write(1, quote, 59);
 	return (1);
 }
