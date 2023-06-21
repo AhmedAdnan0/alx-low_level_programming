f78a1b9206f32ddeab947eb5e0973a2da84113fd
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-06-21 10:57:31 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: num1
+ * @b: num2
+ *
+ * Return: returns the summition of the two nums
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
