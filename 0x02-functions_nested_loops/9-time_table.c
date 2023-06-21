ab169dcdef2468e30014f77e6618fb4d96c121c9
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-06-21 10:48:35 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-time_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - prints every minute of the day
+ */
+
+void times_table(void);
+{
+	int i, j;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		_putchar('0');
+		for (j = 1; j <= 9; ++j)
+		{
+			_putchar(',');
+			_putchar(' ');
+			if (i * j < 10)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('0' + (i * j) / 10);
+			}
+			_putchar('0' + (i * j) % 10);
+		}
+			_putchar('\n');
+	}
+}
