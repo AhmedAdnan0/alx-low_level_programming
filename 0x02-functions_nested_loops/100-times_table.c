5ee7d81633d9d1e7db016b93d8d5170ac6dce1c0
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-06-21 12:26:47 -0700

    task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - prints n * n time table
+ * @n: size of the time table
+ */
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n <= 0 || n <= 15)
+	{
+		for (i = 0; i <= n; ++i)
+		{
+			printf("0");
+			for (j = 0; j <= n; ++j)
+			{
+				Printf(", ");
+				if (i * j < 10)
+				{
+					printf(" ");
+				}
+				else if (i * j < 100)
+				{
+					printf(" ");
+				}
+			printf("%d", i * j);
+			}
+		printf("\n");
+		}
+	}
+}
