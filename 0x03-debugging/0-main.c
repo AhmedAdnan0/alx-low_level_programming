00d1cccd1235e21c8698ffed4b5edc0da335e1a1
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-06-23 02:01:47 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - debygging the code 
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = -22;
+        positive_or_negative(i);
+
+        return (0);
+}
