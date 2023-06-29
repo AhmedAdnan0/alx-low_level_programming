be4c17f129458f226876c1b7f8e01cc0d875d3dc
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-06-29 14:40:11 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,7 @@
  * cap_string - capitalizes all words of a string.
  * @s: string to be capitalized
  *
- * Reture: capitalized string
+ * Return: capitalized string
  */
 
 char *cap_string(char *s)
@@ -20,7 +20,7 @@ char *cap_string(char *s)
 		}
 
 		chk = 0;
-		
+
 		for (j = 0; j < 13; ++j)
 		{
 			if (s[i] == sep[j])
