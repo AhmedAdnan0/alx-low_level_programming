a5e149000f24bab58ba2faf70a7794c9657366d3
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-06-29 11:36:46 -0700

    betty fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,7 +12,9 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (j = 0; dest[j] != '\0'; ++j);
+	for (j = 0; dest[j] != '\0'; ++j)
+	{
+	}
 
 	for (i = 0; src[i] != '\0'; ++i)
 	{
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (j = 0; dest[j] != '\0'; ++j);
+	for (j = 0; dest[j] != '\0'; ++j)
+	{
+	}
 
 	for (i = 0; src[i] != '\0' && i < n; ++i)
 	{
