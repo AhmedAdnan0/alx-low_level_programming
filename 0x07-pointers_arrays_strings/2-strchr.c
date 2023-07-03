cb025b7c505114739c9f02578357dadd0daf6749
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-07-03 01:36:44 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - Returns a pointer to the first occurrence
+ *            of the character c in the string s
+ * @s: pointer
+ * @c: search char
+ *
+ * Return: s    (Success)
+ *         NULL (Failed)
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; ++s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	++s;
+	}
+
+	return (NULL);
+}
