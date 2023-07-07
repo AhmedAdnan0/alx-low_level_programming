7515543cbf9c5348cdfbb20d1ef58fb9797543d9
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-07-07 13:56:42 -0700

    task 100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints minimal coins needed
+ * @argc: argument count
+ * @argv: argument vaalue
+ *
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, c = 0, i;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+
+	if (x < 0)
+		printf("0\n");
+
+	for (i = 0; i < 5; )
+	{
+		while (x - coins[i] >= 0)
+		{
+			++c;
+			x = x - coins[i];
+		}
+		++i;
+	}
+	printf("%d\n", c);
+	return (0);
+}
