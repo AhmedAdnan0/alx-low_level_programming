e48936cba169b7a0ad4e5e6f5ed6252f3b377880
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-07-07 13:08:44 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of 2 nums
+ * @argc: argument count
+ * @argv: argument vaalue
+ *
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 3)
+		return (1);
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
