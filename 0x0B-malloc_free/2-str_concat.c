5555c631170a13f6f304f52c3d272cb35b91c72e
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-07-11 12:51:48 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,23 +12,34 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
+	int i = 0, j = 0;
 	char *ptr;
 
-	for (i = 0; s1[i] != '\0'; )
-		++i;
+	if (s1 != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; )
+			++i;
+	}
+	if (s2 != NULL)
+	{
 	for (j = 0; s2[j] != '\0'; )
 		++j;
-
+	}
 	ptr = malloc(i + j + 1);
 
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i]; ++i)
-		ptr[i] = s1[i];
-	for (j = 0; s2[j]; ++j)
-		ptr[i + j] = s2[j];
+	if (i != 0)
+	{
+		for (i = 0; s1[i]; ++i)
+			ptr[i] = s1[i];
+	}
+	if (j != 0)
+	{
+		for (j = 0; s2[j]; ++j)
+			ptr[i + j] = s2[j];
+	}
 	ptr[i + j] = '\0';
 	return (ptr);
 }
