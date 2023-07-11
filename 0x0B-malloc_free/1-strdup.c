eea3bd4bddae7dd556c6ce422e2f3fefde8bfeb2
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-07-11 11:45:40 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,13 +15,13 @@ char *_strdup(char *str)
 	unsigned int i = 0;
 	char *ptr;
 
+	if (str == NULL)
+		return (NULL);
+
 	for (; str[i]; )
 		++i;
 
-	if (i == 0)
-		return (NULL);
-
-	ptr = malloc(i);
+	ptr = malloc(i - 1);
 
 	if (ptr == NULL)
 		return (NULL);
