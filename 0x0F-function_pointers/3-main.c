08e756505e1d4cdc2d8262428a2d901f981269c9
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-07-19 15:03:12 -0700

    task 3'

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - calculator function
+ * @argc: no. of arguments
+ * @argv: arguments
+ *
+ * Return: (0) SUCCESS
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*s)(int, int);
+
+	if (argc != 4)
+	{
+		printf("ERROR\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	s = get_op_func(argv[2]);
+
+	if (s == 0)
+	{
+		printf("ERROR\n");
+		exit(99);
+	}
+	result = s(a, b);
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - add 2 ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract 2 ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul 2 ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide 2 ints
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("ERROR\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - reminder of division of int a by int b
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("ERROR\n");
+		exit(100);
+	}
+	return (a % b);
+}
+
