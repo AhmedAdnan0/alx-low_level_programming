0b843ad72b2e068129b4d65d196a8f4de1be1b4b
AhmedAdnan0 <a.adnan.m93@gmail.com>
2023-08-09 12:32:40 -0700

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: no of arguments
+ * @argv: file names
+ * Return: 0 if success
+ * Usage: cp file_from file_to
+ */
+
+int main(int argc, char *argv[])
+{
+	int ffd, ftd, r, cf, ct;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	ffd = open(argv[1], O_RDONLY);
+	if (ffd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	ftd = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 00664);
+	if (ftd == -1)
+	{
+		dprintf(2, "Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	r = 1024;
+	while (r == 1024)
+	{
+		r = read(ffd, buffer, 1024);
+		write(ftd, buffer, r);
+	}
+	cf = close(ffd);
+	if (cf == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", ffd);
+		exit(100);
+	}
+	ct = close(ftd);
+	if (ct == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", ftd);
+		exit(100);
+	}
+	return (0);
+}
